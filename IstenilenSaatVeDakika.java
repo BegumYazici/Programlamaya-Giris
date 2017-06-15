29d559fc5d4a9f953c64d4746912186e88e6e7eb
Begüm <yazicibegum@gmail.com>
2017-06-15 17:07:06 +0300

    Create IstenilenSaatVeDakika.java
    by Begüm Yazici

---
+++ IstenilenSaatVeDakika.java
@@ -0,0 +1,35 @@
+package com.example24;
+
+import java.util.Scanner;
+
+/**
+ * Created by asus1 on 15.6.2017.
+ */
+public class examp {
+
+    public static void main(String[] args) {
+
+        Scanner entry = new Scanner(System.in);
+        System.out.print("Saati giriniz: ");
+        int hour1=entry.nextInt();
+        System.out.print("Dakikayi giriniz: ");
+        int minute1=entry.nextInt();
+        System.out.print("Fark(dk) giriniz: ");
+        int differenceMinute=entry.nextInt();
+
+        int minute2=differenceMinute%60;
+        int hour2=differenceMinute/60;
+
+        int sumHour=hour1+hour2;
+        if((sumHour>24)){
+            sumHour = sumHour - 24;
+        }
+
+        int sumMinute=minute1+minute2;
+        if(sumMinute>60){
+            sumMinute=sumMinute-60;
+        }
+
+        System.out.println("Sonuc: "+sumHour+"."+sumMinute);
+    }
+}
