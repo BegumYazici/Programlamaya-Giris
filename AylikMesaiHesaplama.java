888af54423b2430b7813fd44a42eefa2c444adeb
Begüm <yazicibegum@gmail.com>
2017-06-15 17:21:33 +0300

    Create AylikMesaiHesaplama.java
    by Begüm Yazıcı

---
+++ AylikMesaiHesaplama.java
@@ -0,0 +1,35 @@
+package com.example24;
+
+import java.util.Scanner;
+
+/**  
+* Created by asus1 on 15.6.2017.
+ */
+public class examp {
+
+    public static void main(String[] args) {
+
+        Scanner entry = new Scanner(System.in);
+        System.out.print("Saat sayisini giriniz: ");
+        int hour1=entry.nextInt();
+        int mesaiUcreti = 0;
+
+        if(hour1<=10){
+            mesaiUcreti=hour1*1;
+        }
+        else if (hour1<=20){
+            mesaiUcreti=hour1*2;
+        }
+        else if(hour1<=40){
+            mesaiUcreti=hour1*3;
+        }
+        else if(hour1<=100){
+            mesaiUcreti=hour1*4;
+        }
+        else if(hour1>100){
+            mesaiUcreti=hour1*5;
+        }
+
+        System.out.println("Mesai ucretiniz: "+ mesaiUcreti);
+    }
+}
